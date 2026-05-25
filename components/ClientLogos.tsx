f509133b11c97deb48d4937eb1d2fb06cee4de53
thomasvictosin <thomasvictosin@gmail.com>
2026-05-25 18:43:26 +0100

    Increase client logo sizes across all devices

--- components/ClientLogos.tsx
+++ components/ClientLogos.tsx
@@ -3,20 +3,21 @@
 import Image from "next/image";
 
 const CLIENT_LOGOS = [
-  { src: "/images/Swiftcare.png", alt: "Swiftcare" },
-  { src: "/images/Sellmart.png", alt: "Sellmart" },
-  { src: "/images/Mozisha.png", alt: "Mozisha" },
-  { src: "/images/Arctekton.png", alt: "Arctekton" },
-  { src: "/images/Spicynovel.png", alt: "Spicynovel" },
-  { src: "/images/Tastewiz.png", alt: "Tastewiz" },
-  { src: "/images/Tirob.png", alt: "Tirob" },
-  { src: "/images/JMM.png", alt: "JMM" },
+  { src: "/images/Swiftcare2x.png", alt: "Swiftcare" },
+  { src: "/images/Sellmart2x.png", alt: "Sellmart" },
+  { src: "/images/Mozisha2x.png", alt: "Mozisha" },
+  { src: "/images/Arctekton2x.png", alt: "Arctekton" },
+  {src: "/images/Blazer2x.png", alt: "Databliz" },
+  { src: "/images/Spicynovel2x.png", alt: "Spicynovel" },
+  { src: "/images/Tastewiz2x.png", alt: "Tastewiz" },
+  { src: "/images/Tirob2x.png", alt: "Tirob" },
+  { src: "/images/JMM2x.png", alt: "JMM" },
 ] as const;
 
 function LogoCard({ src, alt }: { src: string; alt: string }) {
   return (
-    <div className="client-logo-card relative mx-3 h-[50px] w-[150px] flex-shrink-0 sm:h-14 sm:w-[180px]">
-      <Image src={src} alt={alt} fill sizes="180px" className="object-contain p-1" />
+    <div className="client-logo-card relative mx-3 h-[60px] w-[180px] flex-shrink-0 sm:h-[80px] sm:w-[220px] lg:h-24 lg:w-[280px]">
+      <Image src={src} alt={alt} fill sizes="280px" className="object-contain p-1" />
     </div>
   );
 }
